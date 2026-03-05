a30a92582a4e231409575382faede4bd3ff5a7f5
av3r6un <av3rgun@yandex.com>
2026-03-05 17:12:04 +0300

    Fixed auth middleware

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -22,7 +22,7 @@
     <div class="header_nav">
       <div class="header_nav-item logo">
         <NuxtLink to="/" class="base_link">
-          НАС Кристал
+          {{ settings.general.site_name }}
         </NuxtLink>
       </div>
       <div class="header_navbar">
